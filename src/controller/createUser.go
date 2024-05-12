7ac54667d6c8cd83aca9bf210ca8db34ce3f0ca7
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-05-11 21:19:14 -0300

    feat: loggers

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,33 +1,43 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/MatheusMangueira/go/src/configuration/logger"
 	"github.com/MatheusMangueira/go/src/configuration/validation"
 	"github.com/MatheusMangueira/go/src/model/request"
 	"github.com/MatheusMangueira/go/src/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
 
+	logger.Info("Init Create User Controller",
+		zap.String("journey", "CreateUser"))
+
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+
+		logger.Error("Error trying to validate user info ", err,
+			zap.String("journey", "CreateUser"))
+
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
 	response := response.UserResponse{
 		ID:    "test",
 		Email: userRequest.Email,
 		Name:  userRequest.Name,
 		Age:   userRequest.Age,
 	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "CreateUser"))
 
 	c.JSON(http.StatusOK, response)
 
